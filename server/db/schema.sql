db1431033ca7c145a1f320d36aa5851aaf48eeaf
Fisnik <fiskryeziu@gmail.com>
2025-06-10 18:35:14 +0200

    update schema, needed for checkout

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -30,15 +30,25 @@ CREATE TABLE products (
 
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    user_id UUID REFERENCES users(id),
-    total_price DECIMAL(10, 2),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    stripe_payment_intent_id VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    shipping_address VARCHAR(255) NOT NULL,
+    shipping_country VARCHAR(100) NOT NULL,
+    shipping_city VARCHAR(100) NOT NULL,
+    shipping_zip VARCHAR(20) NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    order_status VARCHAR(50) NOT NULL DEFAULT 'pending',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE order_items (
     id SERIAL PRIMARY KEY,
-    order_id INT REFERENCES orders(id),
-    product_id INT REFERENCES products(id),
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL REFERENCES products(id),
     quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
